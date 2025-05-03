ffef0abd735bc7653ecc7c5d2a634d0c3953a288
sussshi <sus.prisha@gmail.com>
2025-05-04 00:31:45 +0530

    voice recognition untested

--- dementia_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dementia_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,11 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
+import flutter_tts
 import google_sign_in_ios
 import path_provider_foundation
 import sign_in_with_apple
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -20,7 +22,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
